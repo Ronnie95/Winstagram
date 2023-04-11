3ee205903b07d194b944bca35f6f18f585ca69eb
larrytu327 <larrytu327@gmail.com>
2023-04-11 16:40:11 -0700

    added delete route for users

--- controllers/users.js
+++ controllers/users.js
@@ -102,4 +102,24 @@ router.put('/:id', async (req, res, next) => {
     }
 })
 
+router.get('/:id/delete', async (req, res, next) => {
+    try {
+        const userToBeDeleted = await Users.findById(req.params.id);
+        res.render('users/delete', {user: userToBeDeleted})
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedItem = await Users.findByIdAndDelete(req.params.id);
+        res.redirect('/users');
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+})
+
 module.exports = router;
